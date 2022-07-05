1408b6332da49065d61c17b43a2c6cc1bab8976d
vijjini19 <107198946+vijjini19@users.noreply.github.com>
2022-07-05 21:26:42 +0530

    Add files via upload

---
+++ Escape_sequences_part2.py
@@ -0,0 +1,41 @@
+# print("hello\nworld")
+
+# print('hello wor\'ld')  #hello wor'ld
+
+# print("hello wor\"ld")  #hello wor"ld 
+
+# print('c:\Users\test\Desktop\PY9\Escape_sequences_part2')  #unicode error
+# print('c:\\Users\Desktop\PY9\Escape_sequences_part2')
+
+# print('F:\Uatch_9_15_AM')  #unicode error
+# print('F:\\Uatch_9_15_AM')  
+
+# print('c:\\Users\\SINDHU')
+
+# print('\u0001')
+# print('\u0002')
+# print('\u0003')
+# print('\u0004')
+# print('\u0005')
+# print('\u0006')
+# print('\u0007')
+# print('\u0008')
+# print('\U0001f601')
+# print('\u0001f602')
+# print('\u0001f603')
+# print('\u0001f604')
+# print('\u0001f605')
+
+# import emoji
+
+# print(emoji.emojize(":grinning_face_with_big_eyes:"))
+# print(emoji.emojize(":winking_face_with_tongue:"))
+# print(emoji.emojize(":zipper-mouth_face:"))
+# print(emoji.demojize("🤣"))
+# print(emoji.demojize("😎"))
+# print(emoji.demojize("😍"))
+
+'''we can get emojis from this website:htttps://getemoji.com/'''
+
+
+
